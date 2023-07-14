a10ba194cf498a6fb97d2af2cfd80eebab139fa5
Amber Horvath <ambear9@gmail.com>
2023-07-14 14:21:08 -0400

    formatter

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -4,3 +4,7 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  console.log('Current Tab:', tabs[0].url);
+});
